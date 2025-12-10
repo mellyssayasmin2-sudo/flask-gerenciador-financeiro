433c7f080ffa8c4784635bb4a7ba539c11180ad3
mellyssayasmin2-sudo <mellyssayasmin2@gmail.com>
2025-12-10 20:17:44 -0300

    mudanças v5

--- src/config.py
+++ src/config.py
@@ -1,6 +1,12 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///expenses.db')
+    SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
+
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL or "sqlite:///expenses.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
