3458f32b4951ca6524c6bba25fd6efc4fdb3c321
mellyssayasmin2-sudo <mellyssayasmin2@gmail.com>
2025-12-10 20:06:07 -0300

    mudanças v4

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import os
 from flask import Flask
+from src.db import db
+from src.config import Config
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     from src.routes.auth import auth_routes
     from src.routes.expenses import expense_routes
